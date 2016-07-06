0bc3ffa8fa29ffb0cf9664f1c1898f368e140b1f
alexsup <alex@supmenow.com>
2016-07-06 11:38:52 +0000

    Adding data types

---
+++ data-types.py
@@ -0,0 +1,13 @@
+import sys
+
+i = 4
+d = 4.0
+s = 'Yo Yo Yo '
+
+ii = int(sys.stdin.readline())
+dd = float(sys.stdin.readline())
+ss = sys.stdin.readline()
+
+print(i + ii)
+print(d + dd)
+print(s + ss)
